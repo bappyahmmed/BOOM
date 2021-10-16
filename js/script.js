9ca967428d9c701224a3815d0991fa31aaf176c8
bappyahmmed <rokenraja420@gmaol.com>
2021-10-16 13:43:36 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+$(function(){
+
+'use strict'
+
+// banner-slaider
+$('#banner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    dots:true,
+    arrows:false,
+  });
+// banner-slaider
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
